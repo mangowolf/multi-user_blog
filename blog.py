f5e66aa6f057dfd7526056f9e608945aade4bf09
mangowolf <jimmyz949@hotmail.com>
2017-01-24 19:48:56 -0800

    Basic working blog with no styling

--- blog.py
+++ blog.py
@@ -23,24 +23,42 @@ def render_str(template, **params):
 def make_secure_val(val):
 	return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
+def check_secure_val(secure_val):
+	val = secure_val.split('|')[0]
+	if secure_val == make_secure_val(val):
+		return val
+
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
 
 	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
+		#t = jinja_env.get_template(template)
+		params['user'] = self.user
+		return render_str(template, **params)
 
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
 	def set_secure_cookie(self, name, val):
 		cookie_val = make_secure_val(val)
-		self.response.headers.add_header('Set-Cookie',
-										'%s=%s; Path=/' % (name, cookie_val))
+		self.response.headers.add_header('Set-Cookie','%s=%s; Path=/' % (name, cookie_val))
+
+	def read_secure_cookie(self, name):
+		cookie_val = self.request.cookies.get(name)
+		return cookie_val and check_secure_val(cookie_val)
 
 	def login(self, user):
 		self.set_secure_cookie('user_id', str(user.key().id()))
+
+	def logout(self):
+		self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+	def initialize(self, *a, **kw):
+		webapp2.RequestHandler.initialize(self, *a, **kw)
+		uid = self.read_secure_cookie('user_id')
+		self.user = uid and User.by_id(int(uid))
+
 
 def render_post(response, post):
 	response.out.write('<b>' + post.subject + '</b><br>')
@@ -60,6 +78,10 @@ def make_pw_hash(name, pw, salt = None):
 	h = hashlib.sha256(name + pw + salt).hexdigest()
 	return '%s,%s' % (salt, h)
 
+def valid_pw(name, password, h):
+	salt = h.split(',')[0]
+	return h == make_pw_hash(name, password, salt)
+
 def users_key(group = 'default'):
 	return db.Key.from_path('users', group)
 
@@ -69,8 +91,12 @@ class User(db.Model):
 	email = db.StringProperty()
 
 	@classmethod
+	def by_id(cls,uid):
+		return User.get_by_id(uid, parent = users_key())
+
+	@classmethod
 	def by_name(cls,name):
-		u = User.all().filter('name=', name).get()
+		u = User.all().filter('name =', name).get()
 		return u
 
 	@classmethod
@@ -120,9 +146,15 @@ class PostPage(Handler):
 
 class NewPost(Handler):
 	def get(self):
-		self.render("newpost.html")
+		if self.user:
+			self.render("newpost.html")
+		else:
+			self.redirect("/login")
 
 	def post(self):
+		if not self.user:
+			self.redirect('/blog')
+
 		subject = self.request.get('subject')
 		content = self.request.get('content')
 
@@ -217,12 +249,28 @@ class Login(Handler):
 	def get(self):
 		self.render('login-form.html')
 
-	#def post(self):
+	def post(self):
+		username = self.request.get('username')
+		password = self.request.get('password')
+
+		u = User.login(username, password)
+		if u:
+			self.login(u)
+			self.redirect('/blog')
+		else:
+			msg = 'Invalid login'
+			self.render('login-form.html', error = msg)
+
+class Logout(Handler):
+	def get(self):
+		self.logout()
+		self.redirect('/blog')
 
 app = webapp2.WSGIApplication([('/', MainPage),
 								('/blog/?', BlogFront),
 								('/blog/([0-9]+)', PostPage),
 								('/blog/newpost', NewPost),
-								('/blog/signup', Register),
-								('/blog/login', Login)
+								('/signup', Register),
+								('/login', Login),
+								('/logout', Logout),
 								], debug=True)
