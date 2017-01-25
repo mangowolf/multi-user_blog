f4b2399e9052809292bb15cd8e434c7bbbfd543f
mangowolf <jimmyz949@hotmail.com>
2017-01-24 19:58:42 -0800

    Updated css styling

--- blog.py
+++ blog.py
@@ -12,8 +12,7 @@ import jinja2
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-								autoescape = True)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
 
 secret = 'all your base belong to us'
 def render_str(template, **params):
